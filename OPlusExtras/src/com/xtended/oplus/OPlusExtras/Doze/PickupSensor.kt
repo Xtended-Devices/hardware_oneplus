Mrick343/hardware_oplus_test

hardware: OPlusExtras 7.5 import - Now using a better resource based configuration method to allow   universally any Oplus device to ship this package and configure it   by using overlays in their device specific tree.
Co-authored-by: Max Weninger <max.weninger@gmail.com>
Co-authored-by: oddlyspaced <oddlyspaced1@gmail.com>
Co-authored-by: TheScarastic <warabhishek@gmail.com>
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: lostark13 <tushar9915@gmail.com>
Co-authored-by: cristianvaz <cristianvaz81993@gmail.com>
Co-authored-by: jhenrique09 <jhsv09@gmail.com>
Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: Vachounet <vachounet@live.fr>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
Co-authored-by: Michele Bono <bono.michele94@gmail.com>
Co-authored-by: LibXZR <xzr467706992@163.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: pri0818 <priyanshusinghal0818@gmail.com>
Co-authored-by: Singh <sonal.singh.19993@gmail.com>
Co-authored-by: Joey Huab <joey@evolution-x.org>
Signed-off-by: AnierinB <anierin@evolution-x.org>
Signed-off-by: mukesh22584 <mks22584@gmail.com>

OPlusExtras/src/com/xtended/oplus/OPlusExtras/Doze/PickupSensor.kt
@@ -1,0 +1,79 @@
+/*
+ * Copyright (C) 2021-2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.xtended.oplus.OPlusExtras.doze;
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.PowerManager
+import android.os.SystemClock
+import android.util.Log
+
+import java.util.concurrent.Executors
+
+class PickupSensor(
+    private val context: Context, sensorType: String, private val sensorValue: Float
+) : SensorEventListener {
+    private val powerManager = context.getSystemService(PowerManager::class.java)
+    private val wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG)
+
+    private val sensorManager = context.getSystemService(SensorManager::class.java)
+    private val sensor = DozeUtils.getSensor(sensorManager, sensorType)
+
+    private val executorService = Executors.newSingleThreadExecutor()
+    private var entryTimestamp = 0L
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (DEBUG) Log.d(TAG, "Got sensor event: ${event.values[0]}")
+        val delta = SystemClock.elapsedRealtime() - entryTimestamp
+        if (delta < MIN_PULSE_INTERVAL_MS) {
+            return
+        }
+        entryTimestamp = SystemClock.elapsedRealtime()
+        if (event.values[0] == sensorValue) {
+            if (DozeUtils.isPickUpSetToWake(context)) {
+                wakeLock.acquire(WAKELOCK_TIMEOUT_MS)
+                powerManager.wakeUp(
+                    SystemClock.uptimeMillis(), PowerManager.WAKE_REASON_GESTURE, TAG
+                )
+            } else {
+                DozeUtils.launchDozePulse(context)
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
+    fun enable() {
+        if (sensor != null) {
+            Log.d(TAG, "Enabling")
+            executorService.submit {
+                entryTimestamp = SystemClock.elapsedRealtime()
+                sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+            }
+        }
+
+    }
+
+    fun disable() {
+        if (sensor != null) {
+            Log.d(TAG, "Disabling")
+            executorService.submit {
+                sensorManager.unregisterListener(this, sensor)
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "PickupSensor"
+        private const val DEBUG = false
+
+        private const val MIN_PULSE_INTERVAL_MS = 2500L
+        private const val WAKELOCK_TIMEOUT_MS = 300L
+    }
+}

Xtended-Devices/hardware_oneplus

OPlusExtras/src/com/xtended/oplus/OPlusExtras/Doze/PickupSensor.kt
@@ -1,0 +1,79 @@
+/*
+ * Copyright (C) 2021-2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.xtended.oplus.OPlusExtras.doze;
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.PowerManager
+import android.os.SystemClock
+import android.util.Log
+
+import java.util.concurrent.Executors
+
+class PickupSensor(
+    private val context: Context, sensorType: String, private val sensorValue: Float
+) : SensorEventListener {
+    private val powerManager = context.getSystemService(PowerManager::class.java)
+    private val wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG)
+
+    private val sensorManager = context.getSystemService(SensorManager::class.java)
+    private val sensor = DozeUtils.getSensor(sensorManager, sensorType)
+
+    private val executorService = Executors.newSingleThreadExecutor()
+    private var entryTimestamp = 0L
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (DEBUG) Log.d(TAG, "Got sensor event: ${event.values[0]}")
+        val delta = SystemClock.elapsedRealtime() - entryTimestamp
+        if (delta < MIN_PULSE_INTERVAL_MS) {
+            return
+        }
+        entryTimestamp = SystemClock.elapsedRealtime()
+        if (event.values[0] == sensorValue) {
+            if (DozeUtils.isPickUpSetToWake(context)) {
+                wakeLock.acquire(WAKELOCK_TIMEOUT_MS)
+                powerManager.wakeUpWithProximityCheck(
+                    SystemClock.uptimeMillis(), PowerManager.WAKE_REASON_GESTURE, TAG
+                )
+            } else {
+                DozeUtils.launchDozePulse(context)
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
+    fun enable() {
+        if (sensor != null) {
+            Log.d(TAG, "Enabling")
+            executorService.submit {
+                entryTimestamp = SystemClock.elapsedRealtime()
+                sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+            }
+        }
+
+    }
+
+    fun disable() {
+        if (sensor != null) {
+            Log.d(TAG, "Disabling")
+            executorService.submit {
+                sensorManager.unregisterListener(this, sensor)
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "PickupSensor"
+        private const val DEBUG = false
+
+        private const val MIN_PULSE_INTERVAL_MS = 2500L
+        private const val WAKELOCK_TIMEOUT_MS = 300L
+    }
+}